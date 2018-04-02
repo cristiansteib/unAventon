c4cc6a59c1d02e85f2ff52e1fc5a38b8d54ee2da
Cristian Steib <cristiansteib@gmail.com>
2018-04-01 21:34:17 -0300

    nuevas vistas. se pueden registar usuarios desde la web se puede loguear y desloguear usuario desde la web

--- unAventonApp/urls.py
+++ unAventonApp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, login, signIn
+from .views import index, login, signIn, logout, signInRegister
 
 name = 'unAventonApp'
 urlpatterns = [
     path('', index, name='index'),
     path('login', login, name='login'),
     path('signin', signIn, name='signin'),
+    path('signinReg', signInRegister, name='signin_register'),
+    path('logout',logout, name='logout')
+
 ]
--- unAventonApp/views.py
+++ unAventonApp/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, redirect
+from django.contrib.auth import logout as __logout, login as __login, authenticate
+from django.contrib.auth.models import User
 
-# Create your views here.
 def index(request):
-    return render(request,'unAventonApp/index.html')
+    return render(request, 'unAventonApp/index.html')
+
 
 def login(request):
-    return render(request,'unAventonApp/login.html')
+    context = {}
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            ## se autentico bien
+            __login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            ## algun dato esta mal
+            context['error'] = True
+
+    return render(request, 'unAventonApp/login.html', context)
+
 
 def signIn(request):
     return render(request, 'unAventonApp/signIn.html')
+
+
+def signInRegister(request):
+    if request.method == 'POST':
+        r = request.POST
+        user = User.objects.create_user(r['email'],r['email'],r['password'])
+        user.save()
+        return render(request, 'unAventonApp/signin_success.html')
+    return HttpResponseRedirect('signin')
+
+def logout(request):
+    __logout(request)
+    return HttpResponseRedirect('/')
