b75b205adcf1091c7ece1f8139badecec40125a4
Cristian Steib <steibkhriz@gmail.com>
2018-04-14 17:17:10 -0300

    fix serve static files

--- unAventon/settings.py
+++ unAventon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1+26k#$ve_weyn@4b%p56db+rx4yp)!$qgyqn#y82^a2olfhb2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
