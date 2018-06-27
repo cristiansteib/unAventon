dc0ace221dd1aa0a572b5e2240294d7bcdcfe2b1
Cristian Steib <steibkhriz@gmail.com>
2018-06-26 21:57:27 -0300

    manda monto reintegrado

--- runTest.sh
+++
@@ -1,2 +0,0 @@
-#ex: python manage.py test -v2 unAventonApp.tests.TestMetodosDeModels.test_piloto_tiene_calificaciones_pendientes_para_copilotos_de_su_viaje
-python manage.py test -v2 unAventonApp.tests.$1.$2
